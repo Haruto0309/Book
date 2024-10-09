d71bf8489934fcb48f0377e83cf09917c81e311a
EC2 Default User <ec2-user@ip-172-31-16-79.ap-northeast-1.compute.internal>
2024-10-09 08:04:26 +0000

    [FINISH] flash-message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @books = @user.books
+    @new_book = Book.new
   end
 
   def edit
@@ -16,8 +17,12 @@ class UsersController < ApplicationController
 
   def update
     user = current_user
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(user.id)
+    else
+      render edit_user_path(user.id)
+    end
   end
 
   private
