cbdb078dd62d25ba70cac83e1904198d52f2f085
EC2 Default User <ec2-user@ip-172-31-16-79.ap-northeast-1.compute.internal>
2024-10-12 10:46:12 +0000

    [FINISH] 課題4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#top"
   get "home/about", as:"about"
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
